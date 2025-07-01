c1d948282554778e72ac5befa7b3fd2777baf3b4
Richard Olson <richard@richardolson.org>
2025-07-01 17:00:31 -0500

    Refactor login logic in authRouter to use request body parameters and improve validation checks

--- src/services/auth-service.ts
+++ src/services/auth-service.ts
@@ -3,6 +3,7 @@ import { TokenPayload } from "../model/shared-models/token-payload.model";
 import { LogDbService } from "../database/log-db.service";
 import { getNormalizedWebsite } from "../utils/url-match.utils";
 import { AuthDbService } from "../database/auth-db.service";
+import { ObjectId } from "mongodb";
 
 
 export class AuthService {
@@ -15,4 +16,68 @@ export class AuthService {
         }
     }
 
+
+    // /** Attempts to validate a user, and if they exist, returns an ID for them.  If not, then returns undefined. */
+    // async login_FIX_ME(userName: string, webSite: string): Promise<TokenPayload | undefined> {
+    //     // Make sure the website is proper.
+    //     webSite = getNormalizedWebsite(webSite);
+
+    //     await this.loggingService.logMessage({
+    //         level: "info", message: `User attempted to log in: ${userName}, ${webSite}`,
+    //         data: {
+    //             user: userName,
+    //             webSite: webSite
+    //         }
+    //     });
+
+    //     // Ensure we have valid values.
+    //     if (!isValidString(userName) || !isValidString(webSite)) {
+    //         await this.loggingService.logMessage({
+    //             level: 'error', message: `Failed user login: ${userName}, ${webSite}`,
+    //             data: {
+    //                 user: userName,
+    //                 webSite: webSite
+    //             }
+    //         });
+    //         return undefined;
+    //     }
+
+    //     const lcWebsite = webSite.toLowerCase();
+    //     const lcUserName = userName.toLowerCase();
+
+    //     // Attempt to find the user.
+    //     let userInfo = await this.dbService.getUserByNameAndSite(lcUserName, lcWebsite);
+    //     if (userInfo?.company && !userInfo?.user) {
+    //         // If we don't have a user, then we must create one.
+    //         userInfo.user = await this.dbService.createUser(lcUserName, lcWebsite);
+    //     }
+
+    //     // Return the user's ID.
+    //     const result = userInfo?.user ? {
+    //         userId: userInfo.user._id,
+    //         website: userInfo.company.website,
+    //         companyName: userInfo.company.name,
+    //         name: userInfo.user.userName,
+    //         isAdmin: userInfo.user.isAdmin
+    //     } : undefined;
+
+    //     await this.loggingService.logMessage({
+    //         level: 'info',
+    //         message: `${userName} logged in successfully.`,
+    //         data: result
+    //     });
+
+    //     return result;
+    // }
+
+    async login(userName: string, password: string): Promise<TokenPayload | undefined> {
+        throw new Error(`userId isn't implemented properly yet.  This needs to be fixed before we can call this method.`);
+        return {
+            userId: new ObjectId(),
+            companyName: '',
+            name: 'Richard',
+            website: '',
+            isAdmin: true
+        };
+    }
 }
